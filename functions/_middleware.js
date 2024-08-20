df5203fe888578b1c42327d4a01a7d88c2fdd1da
will bell <willbell0@icloud.com>
2024-08-20 13:30:00 -0700

    added updates

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,28 +1,28 @@
 // Source and credits to: https://github.com/pew/cloudflare-pages-social-preview
 class ElementHandler {
   constructor(ogtag) {
-    this.ogtag = ogtag
+    this.ogtag = ogtag;
   }
   element(element) {
-    element.append(this.ogtag, { html: true })
+    element.append(this.ogtag, { html: true });
   }
 }
 
 export async function onRequest(context) {
-  const { request, next } = context
-  const res = await next()
-  const { searchParams, pathname } = new URL(request.url)
+  const { request, next } = context;
+  const res = await next();
+  const { searchParams, pathname } = new URL(request.url);
 
-  if (!(pathname === '/index.html' || pathname === '/')) {
-    return res
+  if (!(pathname === "/index.html" || pathname === "/")) {
+    return res;
   }
 
   // Metatags Variables (edit these!)
-  const metatitle = "Cloudflare Pages Template"
-  const metadescription = "Get started with Cloudflare Pages by deploying this pretty simple template on your own account, and start playing around."
+  const metatitle = "KB Labs";
+  const metadescription = "KB Labs development agency website";
 
-  let name = searchParams.get('myQuery')
-  let ogtag
+  let name = searchParams.get("myQuery");
+  let ogtag;
 
   // these are the metatags we want to inject into the site
   ogtag = `
@@ -31,7 +31,7 @@ export async function onRequest(context) {
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="${request.url}" />
-    <meta property="og:image" content="${request.url}img/preview.png?${name ? 'myQuery=' + name : 'default'}" />
+    <meta property="og:image" content="${request.url}img/preview.png?${name ? "myQuery=" + name : "default"}" />
 
     <meta property="og:image:height" content="630" />
     <meta property="og:image:width" content="1200" />
@@ -41,7 +41,9 @@ export async function onRequest(context) {
     <meta name="twitter:description" content="${metadescription}" />
 
     <meta name="description" content="and even more stuff about my page" />
-  `
+  `;
 
-  return new HTMLRewriter().on('head', new ElementHandler(ogtag)).transform(res)
+  return new HTMLRewriter()
+    .on("head", new ElementHandler(ogtag))
+    .transform(res);
 }
